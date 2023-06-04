26f546176636b2f50354d6efa3dbb757eec768b7
Jihoon Song <jihoonsong@Jihoons-MacBook-Pro.local>
2023-06-04 11:25:15 +0900

    tx

--- src/components/transfer.tsx
+++ src/components/transfer.tsx
@@ -1,40 +1,42 @@
-import React from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-import Background from '../layouts/background'
+import { useLocation, useNavigate } from "react-router-dom";
+import Background from "../layouts/background";
 
-import {getBalanceOf} from '../web3/connect_aa';
-import { callExactInputSingle } from '../web3/entryPoint';
-import web3 from 'web3';
+import web3 from "web3";
+import { callExactInputSingle } from "../web3/entryPoint";
 
 export default function Transfer() {
   const navigate = useNavigate();
   const location = useLocation();
 
   const onClickBack = () => {
-    navigate('/execute')
-  }
+    navigate("/execute");
+  };
 
   const onClickTransfer = () => {
-    getBalanceOf(process.env.PUBLIC_KEY || "", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
-    callExactInputSingle(
-      "0xf5b509bb0909a69b1c207e495f687a596c168e12",
-      web3.utils.toWei("0.01", "ether"), // amountIn (0.01 ETH)
-      "0", // amountOutMin (minimium amount of output token you want to receive - set to 0 for no minimum)
-      "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // tokenIn (wMATIC)
-      "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", // tokenOut (wETH)
-    )
-  }
+    // getBalanceOf(process.env.PUBLIC_KEY || "", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174")
+    try {
+      callExactInputSingle(
+        "0xf5b509bb0909a69b1c207e495f687a596c168e12",
+        web3.utils.toWei("0.01", "ether"), // amountIn (0.01 ETH)
+        "0", // amountOutMin (minimium amount of output token you want to receive - set to 0 for no minimum)
+        "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // tokenIn (wMATIC)
+        "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619" // tokenOut (wETH)
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   const getChainName = (chain: string) => {
     switch (chain) {
       case "polygon":
-        return "Polygon"
+        return "Polygon";
       case "gnosis":
-        return "Gnosis Chain"
+        return "Gnosis Chain";
       case "near":
-        return "Aurora / Near"
+        return "Aurora / Near";
     }
-  }
+  };
 
   return (
     <Background>
@@ -43,37 +45,64 @@ export default function Transfer() {
           <h3 className="text-3xl text-gray-700 font-semibold text-center">
             Swap on {getChainName(location.state.chain)} 🚀
           </h3>
-
         </div>
-        <div className='flex items-center mx-2 flex'>
-            <label className="block mb-2 text-lg font-medium text-gray-700  w-1/6">From</label>
-            <div className="bg-pink-100 border border-pink-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 ml-10">
-              <img src="https://seeklogo.com/images/P/polygon-matic-logo-86F4D6D773-seeklogo.com.png" alt="matic" width={30} height={30} className="inline pr-2"></img>
-              MATIC
-            </div>
+        <div className="flex items-center mx-2 flex">
+          <label className="block mb-2 text-lg font-medium text-gray-700  w-1/6">
+            From
+          </label>
+          <div className="bg-pink-100 border border-pink-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 ml-10">
+            <img
+              src="https://seeklogo.com/images/P/polygon-matic-logo-86F4D6D773-seeklogo.com.png"
+              alt="matic"
+              width={30}
+              height={30}
+              className="inline pr-2"
+            ></img>
+            MATIC
+          </div>
         </div>
-        <div className='flex items-center mx-2 flex'>
-            <label className="block mb-2 text-lg font-medium text-gray-700  w-1/6">To</label>
-            <div className="bg-pink-100 border border-pink-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 ml-10">
-              <img src="https://cryptologos.cc/logos/ethereum-eth-logo.png?v=025" alt="matic" width={30} height={30} className="inline pr-2"></img>
-              WETH
-            </div>
+        <div className="flex items-center mx-2 flex">
+          <label className="block mb-2 text-lg font-medium text-gray-700  w-1/6">
+            To
+          </label>
+          <div className="bg-pink-100 border border-pink-100 text-gray-900 text-sm rounded-lg block w-full p-2.5 ml-10">
+            <img
+              src="https://cryptologos.cc/logos/ethereum-eth-logo.png?v=025"
+              alt="matic"
+              width={30}
+              height={30}
+              className="inline pr-2"
+            ></img>
+            WETH
+          </div>
         </div>
         {/* <div className='flex items-center mx-2 flex'>
             <label className="block mb-2 text-lg font-medium text-gray-700 mr-14  w-1/6">To:</label>
             <input className="bg-pink-100 border border-pink-100 text-gray-900 text-sm rounded-lg block w-full p-2.5" placeholder="0x2f318C334780961FB129D2a6c30D0763d9a5C970" required />
         </div> */}
-        <div className='flex items-center mx-2 my-4 flex'>
-            <label className="block mb-2 text-lg font-medium text-gray-700 mr-3 w-1/6">Amount:</label>
-            <input className="bg-pink-100 border border-pink-100 text-gray-900 text-sm text-center rounded-lg block w-full p-2.5 ml-4" placeholder="0" required />
+        <div className="flex items-center mx-2 my-4 flex">
+          <label className="block mb-2 text-lg font-medium text-gray-700 mr-3 w-1/6">
+            Amount:
+          </label>
+          <input
+            className="bg-pink-100 border border-pink-100 text-gray-900 text-sm text-center rounded-lg block w-full p-2.5 ml-4"
+            placeholder="0"
+            required
+          />
         </div>
-        <button className="py-4 h-1/6 border-4 rounded-2xl hover:bg-pink-100 border-pink-300" onClick={onClickBack}>
+        <button
+          className="py-4 h-1/6 border-4 rounded-2xl hover:bg-pink-100 border-pink-300"
+          onClick={onClickBack}
+        >
           <span className="text-2xl">Back</span>
         </button>
-        <button className="py-8 h-1/6 border-4 rounded-2xl hover:bg-pink-100 border-pink-300" onClick={onClickTransfer}>
+        <button
+          className="py-8 h-1/6 border-4 rounded-2xl hover:bg-pink-100 border-pink-300"
+          onClick={onClickTransfer}
+        >
           <span className="text-2xl">Transfer</span>
         </button>
       </div>
     </Background>
-  )
+  );
 }
--- src/web3/entryPoint.ts
+++ src/web3/entryPoint.ts
@@ -1,108 +1,146 @@
-import abi from "./abi/exactInputSingle.json";
-import Web3 from "web3";
 import HDWalletProvider from "@truffle/hdwallet-provider";
-import ethers from "ethers";
+import Web3 from "web3";
+import abi from "./abi/exactInputSingle.json";
+import entry_point_abi from "./abi/entry_point.json"
+import { BigNumber } from "ethers";
 
-require('dotenv').config();
+require("dotenv").config();
+
+
 
 const privateKey = process.env.PRIVATE_KEY || "";
 const localKeyProvider = new HDWalletProvider({
- privateKeys: [privateKey],
- providerOrUrl: process.env.POLYGON_RPC_URL,
+  privateKeys: [privateKey],
+  providerOrUrl: process.env.POLYGON_RPC_URL,
 });
 const web3 = new Web3(localKeyProvider as any);
 
 const myAccount = web3.eth.accounts.privateKeyToAccount(privateKey);
-const contractAddress = "0x7CaB4088B48a5010e65ad70C7C546372BA7E55DB";
+const entryPointContractAddress = "0x7CaB4088B48a5010e65ad70C7C546372BA7E55DB";
 
 interface WorldIDVerification {
-    root: number
-    group: number
-    signal: string
-    nullifierHash: number
-    appID: string
-    actionID: string
-    proof: number[]
+  root: BigNumber;
+  group: BigNumber;
+  signal: string;
+  nullifierHash: BigNumber;
+  appID: string;
+  actionID: string;
+  proof: BigNumber[];
 }
-  
+
 interface UserOperationVariant {
-    sender: string
-    worldIDVerification: WorldIDVerification
-    callData: string
-    callGasLimit: number
+  sender: string;
+  worldIDVerification: WorldIDVerification;
+  callData: string;
+  callGasLimit: number;
 }
 
 export async function callExactInputSingle(
-  router: string, 
-  amountIn: string, 
-  amountOutMin: string, 
-  tokenIn: string, 
+  router: string,
+  amountIn: string,
+  amountOutMin: string,
+  tokenIn: string,
   tokenOut: string
 ) {
-  const contract = new web3.eth.Contract(abi as any, contractAddress);
+  const entryPointContract = new web3.eth.Contract(entry_point_abi as any, entryPointContractAddress);
 
   // Access the 'cred' data from localStorage
-    const credString = localStorage.getItem('cred');
+  // const credString = localStorage.getItem('cred');
 
-    // Parse the JSON string into an object.
-    const cred = JSON.parse(credString || '');
+  // Parse the JSON string into an object.
+  // const cred = JSON.parse(credString || '');
+  const cred = {
+    merkle_root: BigNumber.from("939610978630742860037106091295265106300874214882050385516927854006536605748"),
+    group: BigNumber.from("1"),
+    signal: "my_signal",
+    nullifier_hash: BigNumber.from("0x0ee07808f36969982d25288cef01767c9eebd1d019a1c3a3ad200c5b5e55cae8"),
+    app_id: "app_5bf8fcd0369d5ac0ec85529e347b5d57",
+    action: "test_2",
+    proof: [
+      BigNumber.from("8542270648952525673731898458107712225662138660430446132942902685213202470602"),
+      BigNumber.from("10018710043607721493297532264810949540821135715131618350961719158348399205787"),
+      BigNumber.from("609530776627107284978967943175338187228060330896361330064668769694179845889"),
+      BigNumber.from("1315904830230138420980116664361181973915783120777801083684370841268990493717"),
+      BigNumber.from("17791960167675277534998862011614460399937959367175037178685013679925572500534"),
+      BigNumber.from("15647476419771666171651041146270415217180224891003654272431630708105274971736"),
+      BigNumber.from("9561265846941125054549792319126517934630210570900590091660886559124975747143"),
+      BigNumber.from("7078960537229967308071328342225677700177359617790010943795560576263472577718")
+    ],
+  };
 
-    console.log(cred);
+  console.log(cred);
 
-    const worldIDVerif: WorldIDVerification = {
-        root: cred.merkle_root,
-        group: 1, // update this line accordingly, as the group property is not present in the provided cred object
-        signal: cred.signal,
-        nullifierHash: cred.nullifier_hash,
-        appID: cred.app_id,
-        actionID: cred.action,
-        proof: cred.proof, // make sure to convert this to the correct format if it's not a number array
-      }
+  const worldIDVerif: WorldIDVerification = {
+    root: cred.merkle_root,
+    group: cred.group, // update this line accordingly, as the group property is not present in the provided cred object
+    signal: cred.signal,
+    nullifierHash: cred.nullifier_hash,
+    appID: cred.app_id,
+    actionID: cred.action,
+    proof: cred.proof, // make sure to convert this to the correct format if it's not a number array
+  };
 
-  console.log('Starting transaction now');
+  console.log("Starting transaction now");
 
-  console.log('worldIdVerif', worldIDVerif)
+  console.log("worldIdVerif", worldIDVerif);
 
   const gasPrice = await web3.eth.getGasPrice();
-
-  console.log('gasPrice', gasPrice);
   
-  const encodedCallData = contract.methods.exactInputSingle(
-    router,
-    amountIn,
-    amountOutMin,
-    tokenIn,
-    tokenOut,
-  ).encodeABI();
 
-  console.log('encodedCallData', encodedCallData)
+  console.log("gasPrice", gasPrice);
 
-//   const receipt = await web3.eth.sendTransaction({
-//     from: myAccount.address,
-//     to: contractAddress,
-//     data: encodedCallData,
-//     gas: 13_000_000,
-//     gasPrice: gasPrice,
-//   });
+  const walletContractAddress = "0xa5e508f6c18a1b88db465602b2488794991ec247"
+  const walletContract = new web3.eth.Contract(abi as any, walletContractAddress);
 
-//   console.log('TX receipt', receipt);
+  const encodedCallData = walletContract.methods
+    .exactInputSingle(router, amountIn, amountOutMin, tokenIn, tokenOut)
+    .encodeABI();
 
-const userOpCallSwapFunc: UserOperationVariant = {
-    sender: '0xa5e508F6C18A1B88Db465602B2488794991eC247',
+  console.log("encodedCallData", encodedCallData);
+
+  //   const receipt = await web3.eth.sendTransaction({
+  //     from: myAccount.address,
+  //     to: entryPointContractAddress,
+  //     data: encodedCallData,
+  //     gas: 13_000_000,
+  //     gasPrice: gasPrice,
+  //   });
+
+  //   console.log('TX receipt', receipt);
+
+  const userOpCallSwapFunc: UserOperationVariant = {
+    sender: "0xa5e508F6C18A1B88Db465602B2488794991eC247",
     worldIDVerification: worldIDVerif,
     callData: encodedCallData,
-    callGasLimit: 30_000_000,
-}
+    callGasLimit: 2_500_000,
+  };
 
-console.log('myAccount.address', myAccount.address)
+  console.log("myAccount.address", myAccount.address);
 
-const receipt = await web3.eth.sendTransaction({
-    from: myAccount.address,
-    to: contractAddress,
-    data: userOpCallSwapFunc.callData,
-    gas: userOpCallSwapFunc.callGasLimit,
-    gasPrice: gasPrice,
-  });
+  // const entryPointContractAddress = ""
+  // const entryPointContract = new web3.eth.Contract(abi as any, entryPointContractAddress);
 
-  console.log('TX receipt', receipt);
+  const entryPointCalldata = entryPointContract.methods
+    .handleOps([userOpCallSwapFunc])
+    .encodeABI();
+
+
+    // 159662720585 * 3000000
+    // 0.454256772009742581 * 1e18
+
+
+    try {
+      const receipt = await web3.eth.sendTransaction({
+        from: myAccount.address,
+        to: entryPointContractAddress,
+        data: entryPointCalldata,
+        gas: userOpCallSwapFunc.callGasLimit,
+        gasPrice: gasPrice,
+      });
+
+      console.log("TX receipt", receipt);
+    } catch (e) {
+      console.log("error", e);
+    }
+
 }
